8d656ec6acd6c355a27091cd82cfac8b97fd467b
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-08-14 16:25:38 +0200

    Develop why-auj screen

--- app/(tabs)/why-auj.jsx
+++ app/(tabs)/why-auj.jsx
@@ -1,11 +1,32 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ImageBackground, Image } from "react-native";
+import ListItem from "../../components/ListItem";
 
 const WhyAUJ = () => {
   return (
     <SafeAreaView className="flex-1 bg-black">
-      <View className="flex-1 w-full bg-transparent justify-center items-center">
-        <Text className="text-white">Why AUJ</Text>
-      </View>
+      <ImageBackground
+        source={require("../../assets/img/purple_light_why_auj.png")}
+        resizeMode="fit"
+        className="flex-1"
+      >
+        <ImageBackground
+          source={require("../../assets/img/bright_purple_light_why_auj.png")}
+          resizeMode="fit"
+          className="flex-1"
+        >
+          <View className="flex-1 w-full bg-transparent justify-center items-center py-14">
+            <ListItem text="Equípo" />
+            <ListItem text="Intercambio" />
+            <Image
+              source={require("../../assets/img/team.png")}
+              className="rounded-3xl"
+            />
+            <ListItem text="Apollo" />
+            <ListItem text="Aprendizaje" />
+            <ListItem text="Diversión" />
+          </View>
+        </ImageBackground>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
