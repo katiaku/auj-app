b67b7fb33c8be4844f01eea0cd45e2499dae4ca6
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-08-25 18:12:28 +0200

    Add image dragging with react native gesture handler

--- app/index.jsx
+++ app/index.jsx
@@ -5,16 +5,27 @@ import {
   Pressable,
   Animated,
   Easing,
+  StyleSheet,
+  Text,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { useRouter } from "expo-router";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  PanGestureHandler,
+  State,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
 const App = () => {
   const router = useRouter();
+
+  const translateX = useRef(new Animated.Value(0)).current;
+  const translateY = useRef(new Animated.Value(0)).current;
   const shadowAnim = useRef(new Animated.Value(10)).current;
+  const [dropAreaLayout, setDropAreaLayout] = useState(null);
 
   useEffect(() => {
     Animated.loop(
@@ -35,59 +46,115 @@ const App = () => {
     ).start();
   }, [shadowAnim]);
 
+  const onGestureEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationX: translateX,
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: false },
+  );
+
+  const onHandlerStateChange = (event) => {
+    if (event.nativeEvent.state === State.END) {
+      Animated.spring(translateX, {
+        toValue: 0,
+        useNativeDriver: false,
+      }).start();
+
+      Animated.spring(translateY, {
+        toValue: 0,
+        useNativeDriver: false,
+      }).start();
+    }
+  };
+
   return (
-    <SafeAreaView className="flex-1 bg-black">
-      <ImageBackground
-        source={require("../assets/img/purple_light_home.png")}
-        resizeMode="cover"
-        className="w-full h-full"
-      >
-        <View className="flex-1 w-full bg-transparent justify-center items-center">
-          <Animated.View
-            style={{
-              width: 150,
-              height: 150,
-              borderRadius: 20,
-              marginBottom: 32,
-              elevation: shadowAnim, // Android
-              shadowColor: "#fff",
-              shadowOffset: { width: 0, height: 0 },
-              shadowOpacity: 0.9,
-              shadowRadius: shadowAnim,
-            }}
-          >
-            <Image
-              source={require("../assets/img/foto_CV.png")}
-              resizeMode="cover"
-              className="w-full h-full rounded-3xl"
-            />
-          </Animated.View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView className="flex-1 bg-black">
+        <ImageBackground
+          source={require("../assets/img/purple_light_home.png")}
+          resizeMode="cover"
+          className="w-full h-full"
+        >
+          <View className="flex-1 w-full justify-center items-center bg-transparent">
+            <Text
+              className="text-white font-poppins mb-2"
+              style={{ fontFamily: "Poppins_400Regular" }}
+            >
+              (drag)
+            </Text>
+            <PanGestureHandler
+              onGestureEvent={onGestureEvent}
+              onHandlerStateChange={onHandlerStateChange}
+            >
+              <Animated.View
+                style={[
+                  styles.draggableContainer,
+                  {
+                    transform: [{ translateX }, { translateY }],
+                    shadowRadius: shadowAnim,
+                    elevation: shadowAnim,
+                  },
+                ]}
+              >
+                <Image
+                  source={require("../assets/img/foto_CV.png")}
+                  resizeMode="cover"
+                  className="w-full h-full rounded-[30px]"
+                />
+              </Animated.View>
+            </PanGestureHandler>
 
-          <FontAwesome name="hand-paper-o" size={35} color="white" />
+            <FontAwesome name="hand-paper-o" size={35} color="white" />
 
-          <View className="w-[150px] h-[150px]">
-            <Image
-              source={require("../assets/img/arrow.png")}
-              resizeMode="cover"
-              className="w-[150px] h-[150px] rounded-3xl"
-            />
+            <View className="w-[150px] h-[150px] rounded-[30px]">
+              <Image
+                source={require("../assets/img/arrow.png")}
+                resizeMode="cover"
+                className="w-full h-full rounded-[30px]"
+              />
+            </View>
+
+            <Pressable
+              className="w-[150px] h-[150px] "
+              onLayout={(event) => setDropAreaLayout(event.nativeEvent.layout)}
+              onPress={() => router.push("/why-auj")}
+            >
+              <Image
+                source={require("../assets/img/auj_logo.png")}
+                resizeMode="cover"
+                className="w-full h-full rounded-[30px]"
+              />
+            </Pressable>
+            <Text
+              className="text-white font-poppins mt-2"
+              style={{ fontFamily: "Poppins_400Regular" }}
+            >
+              (tap)
+            </Text>
           </View>
-
-          <Pressable
-            className="w-[150px] h-[150px] rounded-3xl"
-            onPress={() => router.push("/why-auj")}
-          >
-            <Image
-              source={require("../assets/img/auj_logo.png")}
-              resizeMode="cover"
-              className="w-full h-full rounded-3xl"
-            />
-          </Pressable>
-        </View>
-      </ImageBackground>
-      <StatusBar style="light" />
-    </SafeAreaView>
+        </ImageBackground>
+        <StatusBar style="light" />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  draggableContainer: {
+    width: 150,
+    height: 150,
+    borderRadius: 20,
+    marginBottom: 32,
+    shadowColor: "#fff",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.9,
+    zIndex: 99,
+  },
+});
 
 export default App;
